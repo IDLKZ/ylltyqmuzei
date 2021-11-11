a0bda37f957537d4581d9b8fb5701de22a5628f9
Misti <mistier.famous@gmail.com>
2021-11-11 21:54:12 +0600

    §12345

---
+++ lib/screens/single_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SingleModel extends StatelessWidget {
+  const SingleModel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
