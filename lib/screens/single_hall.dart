466e736f2ef4ed136d95a884d7bca5cb593b2317
Misti <mistier.famous@gmail.com>
2021-11-18 23:09:51 +0600

    §12345

--- lib/screens/single_hall.dart
+++ lib/screens/single_hall.dart
@@ -22,12 +22,216 @@ class SingleHalls extends StatefulWidget {
 class _SingleHallsState extends State<SingleHalls> {
   late Future <Hall> hall;
   late int currentIndex = 0;
+  CarouselController buttonCarouselController = CarouselController();
+
+  Widget myCarousel(dynamic snapshot){
+    return CarouselSlider(
+      carouselController: buttonCarouselController,
+      options: CarouselOptions(
+          initialPage: 0,
+          enableInfiniteScroll: false,
+          viewportFraction: 1.0,
+          enlargeCenterPage: false,
+          height: MediaQuery.of(context).size.height
+        // autoPlay: false,
+      ),
+      items: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  image: DecorationImage(
+                      colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.7),
+                          BlendMode.dstATop),
+                      image: NetworkImage(
+                          Mixin().getImage(snapshot.data!.image)),
+                      fit: BoxFit.cover
+                  ),
+                ),
+              ),
+              Container(
+                color: const Color.fromRGBO(
+                    21, 18, 18, 0.18823529411764706),
+              ),
+              SafeArea(
+                  child: Container(
+                    alignment: const Alignment(-1, -1),
+                    child: Card(
+                      color: Colors.black.withOpacity(0.5),
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        color: Colors.white,
+
+                      ),
+                    ),
+                  )
+              ),
+              SafeArea(
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: const Alignment(1, 1),
+                        child: Card(
+                          color: Colors.black.withOpacity(0.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: IconButton(
+                              icon: const Icon(
+                                Icons.account_balance, size: 35,),
+                              onPressed: () {
+                                Navigator.pushNamed(context,"/tours",arguments: snapshot.data!.id.toString());
+                              },
+                              color: Colors.white,
+                              tooltip: "Тур в 360",
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20,),
+                      Container(
+                        alignment: const Alignment(1, 1),
+                        child: Card(
+                          color: Colors.black.withOpacity(0.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: IconButton(
+                              icon: const Icon(
+                                FontAwesomeIcons.cubes, size: 35,),
+                              onPressed: () {
+                                Navigator.pushNamed(context, "/thirdModels",arguments: snapshot.data!.id.toString());
+                              },
+                              color: Colors.white,
+                              tooltip: "Экспонаты",
+                            ),
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  )
+              ),
+              SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        SingleChildScrollView(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              DefaultTextStyle(
+                                style: const TextStyle(
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.bold,
+                                    color:Color(-285354647)
+                                ),
+                                child: AnimatedTextKit(
+                                    totalRepeatCount: 1,
+                                    animatedTexts: [
+                                      TyperAnimatedText(
+                                          snapshot.data!.getTitle()),
+                                    ]
+                                ),
+
+                              ),
+                              const SizedBox(height: 20,),
+                              DefaultTextStyle(
+                                style: const TextStyle(
+                                  fontSize: 16.0,
+                                ),
+                                child: AnimatedTextKit(
+                                  totalRepeatCount: 1,
+                                  animatedTexts: [
+                                    TypewriterAnimatedText(
+                                      snapshot.data!.getDescription(),
+                                      speed: const Duration(milliseconds: 10),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+
+              ),
+
+            ],
+          ),
+        ),
+        Stack(
+
+          children: [
+            Container(
+              decoration:  BoxDecoration(
+                image:  DecorationImage(
+                  image:  NetworkImage(Mixin().getImage(snapshot.data!.image)),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child:  BackdropFilter(
+                filter:  ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                child:  Container(
+                  decoration: BoxDecoration(color: Colors.black.withOpacity(0.5)),
+                ),
+              ),
+            ),
+            Center(
+                child:Youtube(url: snapshot.data!.getVideo(),)
+            ),
+          ],
+        ),
+        Stack(
+          children: [
+            Container(
+              decoration:  BoxDecoration(
+                image:  DecorationImage(
+                  image:  NetworkImage(Mixin().getImage(snapshot.data!.image)),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child:  BackdropFilter(
+                filter:  ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                child:  Container(
+                  decoration: BoxDecoration(color: Colors.black.withOpacity(0.5)),
+                ),
+              ),
+            ),
+            SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(Mixin().getImage(snapshot.data!.image)),
+                  const SizedBox(height: 10,),
+                  const Text("Сейчас играет подкаст к:", style:TextStyle(fontSize: 20, color:Colors.white, fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
+                  Text(snapshot.data!.getTitle(), style:const TextStyle(fontSize: 20, color:Colors.white, fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+                  AudioPlayer(url: snapshot.data!.getAudio(),)
+                ],
+              ),
+            ),
+
+          ],
+        ),
+      ],
+
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     RouteSettings args = ModalRoute.of(context)!.settings;
     String alias = args.arguments as String;
     hall = HallProvider().getSingleHall(alias);
-    CarouselController buttonCarouselController = CarouselController();
 
     return Scaffold(
       extendBody: true,
@@ -36,205 +240,7 @@ class _SingleHallsState extends State<SingleHalls> {
           builder: (context, snapshot) {
             if (!snapshot.hasError) {
               if (snapshot.hasData) {
-                return CarouselSlider(
-                  carouselController: buttonCarouselController,
-                  options: CarouselOptions(
-                      enableInfiniteScroll: false,
-                      viewportFraction: 1.0,
-                      enlargeCenterPage: false,
-                      height: MediaQuery.of(context).size.height
-                    // autoPlay: false,
-                  ),
-                  items: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height,
-                      child: Stack(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              image: DecorationImage(
-                                  colorFilter: ColorFilter.mode(
-                                      Colors.black.withOpacity(0.7),
-                                      BlendMode.dstATop),
-                                  image: NetworkImage(
-                                      Mixin().getImage(snapshot.data!.image)),
-                                  fit: BoxFit.cover
-                              ),
-                            ),
-                          ),
-                          Container(
-                            color: const Color.fromRGBO(
-                                21, 18, 18, 0.18823529411764706),
-                          ),
-                          SafeArea(
-                              child: Container(
-                                alignment: const Alignment(-1, -1),
-                                child: Card(
-                                  color: Colors.black.withOpacity(0.5),
-                                  child: IconButton(
-                                    icon: const Icon(Icons.arrow_back),
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    color: Colors.white,
-
-                                  ),
-                                ),
-                              )
-                          ),
-                          SafeArea(
-                              child: Column(
-                                children: [
-                                  Container(
-                                    alignment: const Alignment(1, 1),
-                                    child: Card(
-                                      color: Colors.black.withOpacity(0.5),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: IconButton(
-                                          icon: const Icon(
-                                            Icons.account_balance, size: 35,),
-                                          onPressed: () {
-                                            Navigator.pushNamed(context,"/tours",arguments: snapshot.data!.id.toString());
-                                          },
-                                          color: Colors.white,
-                                          tooltip: "Тур в 360",
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(height: 20,),
-                                  Container(
-                                    alignment: const Alignment(1, 1),
-                                    child: Card(
-                                      color: Colors.black.withOpacity(0.5),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: IconButton(
-                                          icon: const Icon(
-                                            FontAwesomeIcons.cubes, size: 35,),
-                                          onPressed: () {
-                                            Navigator.pushNamed(context, "/thirdModels",arguments: snapshot.data!.id.toString());
-                                          },
-                                          color: Colors.white,
-                                          tooltip: "Экспонаты",
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-
-                                ],
-                              )
-                          ),
-                          SafeArea(
-                              child: Padding(
-                                padding: const EdgeInsets.all(20),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    SingleChildScrollView(
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.end,
-                                        children: [
-                                          DefaultTextStyle(
-                                            style: const TextStyle(
-                                                fontSize: 24.0,
-                                                fontWeight: FontWeight.bold,
-                                                color:Color(-285354647)
-                                            ),
-                                            child: AnimatedTextKit(
-                                                totalRepeatCount: 1,
-                                                animatedTexts: [
-                                                  TyperAnimatedText(
-                                                      snapshot.data!.getTitle()),
-                                                ]
-                                            ),
-
-                                          ),
-                                          const SizedBox(height: 20,),
-                                          DefaultTextStyle(
-                                            style: const TextStyle(
-                                              fontSize: 16.0,
-                                            ),
-                                            child: AnimatedTextKit(
-                                              totalRepeatCount: 1,
-                                              animatedTexts: [
-                                                TypewriterAnimatedText(
-                                                    snapshot.data!.getDescription(),
-                                                  speed: const Duration(milliseconds: 10),
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              )
-
-                          ),
-
-                        ],
-                      ),
-                    ),
-                    Stack(
-
-                      children: [
-                        Container(
-                          decoration:  BoxDecoration(
-                            image:  DecorationImage(
-                              image:  NetworkImage(Mixin().getImage(snapshot.data!.image)),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          child:  BackdropFilter(
-                            filter:  ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-                            child:  Container(
-                              decoration: BoxDecoration(color: Colors.black.withOpacity(0.5)),
-                            ),
-                          ),
-                        ),
-                        Center(
-                          child:Youtube(url: snapshot.data!.getVideo(),)
-                        ),
-                      ],
-                    ),
-                    Stack(
-                      children: [
-                        Container(
-                          decoration:  BoxDecoration(
-                            image:  DecorationImage(
-                              image:  NetworkImage(Mixin().getImage(snapshot.data!.image)),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          child:  BackdropFilter(
-                            filter:  ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-                            child:  Container(
-                              decoration: BoxDecoration(color: Colors.black.withOpacity(0.5)),
-                            ),
-                          ),
-                        ),
-                       SafeArea(
-                         child: Column(
-                           mainAxisAlignment: MainAxisAlignment.center,
-                           children: [
-                             Image.network(Mixin().getImage(snapshot.data!.image)),
-                             const SizedBox(height: 10,),
-                             const Text("Сейчас играет подкаст к:", style:TextStyle(fontSize: 20, color:Colors.white, fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
-                             Text(snapshot.data!.getTitle(), style:const TextStyle(fontSize: 20, color:Colors.white, fontWeight: FontWeight.bold),textAlign: TextAlign.center),
-                             AudioPlayer(url: snapshot.data!.getAudio(),)
-                           ],
-                         ),
-                       ),
-
-                      ],
-                    ),
-                  ],
-
-                );
+                return myCarousel(snapshot);
               }
               else {
                 return const Center(
@@ -253,7 +259,10 @@ class _SingleHallsState extends State<SingleHalls> {
         currentIndex: currentIndex,
         onTap: (currentInd){setState(() {
           currentIndex = currentInd;
-        });},
+           buttonCarouselController.jumpToPage(currentInd);
+        });
+
+        },
         backgroundColor: Colors.black.withOpacity(0.1),
         selectedItemColor: Colors.amberAccent,
         unselectedItemColor: Colors.white,
