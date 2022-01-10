36cf14f29c55769462ac5804a7e108e4a1af5520
Misti <mistier.famous@gmail.com>
2022-01-10 12:21:35 +0600

    1233

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,10 +3,10 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class Constants{
-  static String apiUrl = "https://jitsi.idl.kz/api/";
-  static String apiRes = "https://jitsi.idl.kz/uploads/";
+  static String apiUrl = "https://muzeum-app.kz/api/";
+  static String apiRes = "https://muzeum-app.kz/uploads/";
   static String defaultImage = "https://avatars.mds.yandex.net/i?id=ffde3955369ab45c93dee00f066a564b-5144307-images-thumbs&ref=rim&n=33&w=480&h=270";
-  static String apiWebUrl = "https://jitsi.idl.kz/model3d/";
+  static String apiWebUrl = "https://muzeum-app.kz/model3d/";
 
   // static const kMainColor = Color(0xFF0c1e34);
   static const kMainColor = Color(0xFFE3AF2A);
